7a412024e5d9fd62769053d8ffc060f74ad56f86
Mahsa Rasti <mahsa.rasti80@gmail.com>
2022-10-21 00:01:33 +0330

    .

--- ca1/q2_madaline.py
+++ ca1/q2_madaline.py
@@ -1,6 +1,19 @@
+from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df=pd.read_csv("E:\seventh_sem\Neural_network\projects\Neural-network-deep-learning-course\ca1\MadaLine.csv")
-print(df.values)
+x=pd.read_csv("E:\\seventh_sem\\Neural_network\\projects\\Neural_network_course\\ca1\\MadaLine.csv", usecols = [0])
+y=pd.read_csv("E:\\seventh_sem\\Neural_network\\projects\\Neural_network_course\\ca1\\MadaLine.csv", usecols = [1])
+z=pd.read_csv("E:\\seventh_sem\\Neural_network\\projects\\Neural_network_course\\ca1\\MadaLine.csv", usecols = [2])
+# df=pd.DataFrame(df0,columns=['x','y','t'])
+
+# print(df.info())
+#plt.figure()
+fig =plt.figure(figsize=(12, 12))
+ax =fig.add_subplot(projection='3d')
+ax.scatter(x,y,z,c='r')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
